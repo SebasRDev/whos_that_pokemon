6489272b54f0f8b343bc76126905d5a7e9ec167b
Sebastian Ramírez <sebasramirez.dev@gmail.com>
2021-09-22 14:38:33 -0500

    add pokemonPicture and pokemonOptions

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Quién es este pokémon?</h1>
+
+  <!-- TODO: img -->
+  <PokemonPicture
+    :pokemonId="4"
+    :showPokemon="true" />
+
+  <!-- TODO: Opciones -->
+  <PokemonOptions />
+
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  }
+
+}
+</script>
